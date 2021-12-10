4c46a122b79311cf1335f7a37f53833a9f5112de
sooof <sooof@me.com>
2021-12-09 20:24:09 -0800

    addItem work && add contexts/ProductContext.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, createContext, useContext } from "react";
+
 import { Route } from 'react-router-dom';
 import data from './data';
+
+import {ProductContext} from "./contexts/ProductContext"
+
+// export const ProductContext =   createContext()
+// console.log("ProductContext", ProductContext)
 
 // Components
 import Navigation from './components/Navigation';
@@ -13,21 +19,26 @@ function App() {
 
 	const addItem = item => {
 		// add the given item to the cart
+		setCart(
+			[...cart,item]
+		)
 	};
 
 	return (
-		<div className="App">
-			<Navigation cart={cart} />
+		<ProductContext.Provider>
+			<div className="App">
+				<Navigation cart={cart} />
 
-			{/* Routes */}
-			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
-			</Route>
+				{/* Routes */}
+				<Route exact path="/">
+					<Products products={products} addItem={addItem} />
+				</Route>
 
-			<Route path="/cart">
-				<ShoppingCart cart={cart} />
-			</Route>
-		</div>
+				<Route path="/cart">
+					<ShoppingCart cart={cart} />
+				</Route>
+			</div>
+		</ProductContext.Provider>
 	);
 }
 
